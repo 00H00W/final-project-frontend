64e0b5f78aaaed51207a0e264dac8cce5984138d
Sam Branham <hotdogsb@gmail.com>
2025-04-15 15:27:13 -0500

    Style footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,12 @@
 import "./Footer.css";
 
 function Footer() {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <p className="footer__text">Developed by Sam Branham</p>
+      <p className="footer__text">{new Date().getFullYear()}</p>
+    </footer>
+  );
 }
 
 export default Footer;
