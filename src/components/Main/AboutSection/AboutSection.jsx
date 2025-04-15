7f7dcd56f173a26db160f4dbc352b21924e6997a
Sam Branham <hotdogsb@gmail.com>
2025-04-15 15:12:57 -0500

    Style about section

--- src/components/Main/AboutSection/AboutSection.jsx
+++ src/components/Main/AboutSection/AboutSection.jsx
@@ -1,16 +1,24 @@
 import "./AboutSection.css";
+import headshot from "../../../assets/headshot-temp.jpeg";
 
 function AboutSection() {
   return (
     <section className="about-section">
-      <h2 className="about-section__heading">About the author</h2>
-      <p className="about-section__body">
-        This block describes the project author. Here you should indicate your
-        name, what you do, and which development technologies you know.
-        <br /> <br />
-        You can also talk about your experience with TripleTen, what you learned
-        there, and how you can help potential customers.
-      </p>
+      <div className="about-section__text-content">
+        <h2 className="about-section__heading">About the author</h2>
+        <p className="about-section__body">
+          This block describes the project author. Here you should indicate your
+          name, what you do, and which development technologies you know.
+          <br /> <br />
+          You can also talk about your experience with TripleTen, what you
+          learned there, and how you can help potential customers.
+        </p>
+      </div>
+      <img
+        className="about-section__headshot"
+        src={headshot}
+        alt="picture of author"
+      />
     </section>
   );
 }
